7c0c82915324117158599e3203f440fb25681539
Grant Heffernan <heffergm@gmail.com>
2013-09-21 06:19:04 -0400

    1

---
+++ Berksfile
@@ -0,0 +1,6 @@
+site :opscode
+
+cookbook 'apt'
+cookbook 'git'
+cookbook 'vpnwrap', :path => '.'
+cookbook 'openvpn', git: 'https://github.com/heffergm/openvpn.git', branch: 'master'
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: vpnwrap
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'openvpn::default'
+
