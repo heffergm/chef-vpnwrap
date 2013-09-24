827e549df055217954e4171ae1fe36b631b21d87
Grant Heffernan <heffergm@gmail.com>
2013-09-24 09:42:06 -0400

    cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,18 +14,18 @@ default[:openvpn][:routes] = [
   "push 'route 10.0.3.0 255.255.255.0'"
 ]
 
-default["openvpn"]["local"]           = node[:ipaddress]
-default["openvpn"]["proto"]           = 'udp'
-default["openvpn"]["port"]            = '1194'
-default["openvpn"]["type"]            = 'server'
-default["openvpn"]["subnet"]          = '10.8.0.0'
-default["openvpn"]["netmask"]         = '255.255.0.0'
-default["openvpn"]["gateway"]         = "vpn.#{node["domain"]}"
-default["openvpn"]["log"]             = '/var/log/openvpn.log'
-default["openvpn"]["key_dir"]         = '/etc/openvpn/keys'
-default["openvpn"]["crl_dir"]         = '/etc/openvpn/crl'
-default["openvpn"]["signing_ca_key"]  = "#{node["openvpn"]["key_dir"]}/ca.key"
-default["openvpn"]["signing_ca_cert"] = "#{node["openvpn"]["key_dir"]}/ca.crt"
-default["openvpn"]["script_security"] = 2
-default["openvpn"]["user"]            = 'nobody'
+default[:openvpn][:local]           = node[:ipaddress]
+default[:openvpn][:proto]           = 'udp'
+default[:openvpn][:port]            = '1194'
+default[:openvpn][:type]            = 'server'
+default[:openvpn][:subnet]          = '10.8.0.0'
+default[:openvpn][:netmask]         = '255.255.0.0'
+default[:openvpn][:gateway]         = "vpn.#{node["domain"]}"
+default[:openvpn][:log]             = '/var/log/openvpn.log'
+default[:openvpn][:key_dir]         = '/etc/openvpn/keys'
+default[:openvpn][:crl_dir]         = '/etc/openvpn/crl'
+default[:openvpn][:signing_ca_key]  = "#{node[:openvpn][:key_dir]}/ca.key"
+default[:openvpn][:signing_ca_cert] = "#{node[:openvpn][:key_dir]}/ca.crt"
+default[:openvpn][:script_security] = 2
+default[:openvpn][:user]            = 'nobody'
 
